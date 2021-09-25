1edb95ad72894ba69d9eb2a8d5b48c6f3fa40b3b
d.murich <d.murich@reply.de>
2021-09-25 10:47:18 +0200

    add terrain enum

--- src/main/java/model/Terrain.java
+++ src/main/java/model/Terrain.java
@@ -1,7 +1,19 @@
 package model;
 
 public enum Terrain {
+    Highway(70, 'H'),
+    Railway(50, 'T'),
+    StandardTerrain(100, '_'),
+    RailwayLevelCrossing(120, 'X'),
+    Dirt(150, '+'),
+    TrafficJam(200, '*'),
+    Water(800, '~'),
+    Mountains(-1, '#');
+    public int value;
+    public char character;
 
-    private int value
-
+    Terrain(int value, char character) {
+        this.value = value;
+        this.character = character;
+    }
 }
