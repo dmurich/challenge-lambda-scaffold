f871bae933aafe15b51552109780d293ec5eeb48
Umberto Sgueglia <umberto.sgueglia@external.fcagroup.com>
2021-09-25 21:23:29 +0200

    adding max rule

--- src/main/java/lambda/SolutionChecker.java
+++ src/main/java/lambda/SolutionChecker.java
@@ -60,8 +60,8 @@ public class SolutionChecker {
 			long serviceScore = customerScore.getServiceUtils() - Utils.getDistance(customerScore.getReplyOffice(),customerScore.getCustomerLocation());
 			finalScore = customerLocalScore + serviceScore;
 		}
-
-		return finalScore - totalBuildingCosts;
+//TODO add the bonus
+		return Math.max(finalScore - totalBuildingCosts, 0);
 	}
 
 
